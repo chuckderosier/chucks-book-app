b7c2cf9cf9ff3d0b8e7eeb6b2413ec8e776b7578
chuckderosier <chuckderosier2018@gmail.com>
2019-08-26 20:15:13 -0400

    created NavBar and UserPage biolers with Routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './components/HomePage.js'
+import UserPage from './components/UserPage.js'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
+          <Route exact path='/user' component={UserPage} />
           <Route path='/' component={HomePage} />
         </Switch>
       </Router>
